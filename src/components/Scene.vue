801e1832703fcf1fe5a9ea2897b3a27ba35c5d5d
Sean Cheung <sean@cotown.net>
2017-05-16 17:45:35 +0800

    fist commit

---
+++ src/components/Scene.vue
@@ -0,0 +1,12 @@
+<template>
+  
+</template>
+
+<script>
+import THREE from 'three';
+import Vue from 'vue';
+
+export default {
+    
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'build.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+}
